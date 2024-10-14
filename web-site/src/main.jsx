01dc91e8560a1228a33820fe83710945ca60cf48
Aleksandr Saidbaev <sa.alleksandr@gmail.com>
2024-10-15 01:56:48 +0200

    Add package Home

--- web-site/src/main.jsx
+++ web-site/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from "./components/header/Header.jsx";
+import Home from "./components/home/Home.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
-  </StrictMode>,
+      <Home/>
+  </StrictMode>
 )
