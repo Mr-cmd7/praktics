cf4bf91c9ff99644ae50363537f0f554b47a32cf
Aleksandr Saidbaev <sa.alleksandr@gmail.com>
2024-10-15 02:44:46 +0200

    add package About

--- web-site/src/main.jsx
+++ web-site/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from "./components/header/Header.jsx";
 import Home from "./components/home/Home.jsx";
+import About from "./components/about/About.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
       <Home/>
+      <About/>
   </StrictMode>
 )
